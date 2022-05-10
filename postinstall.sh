af6f8d82c0127727f89fe92e3d970afe0410e32d
afb12345 <62421730+afb12345@users.noreply.github.com>
2022-05-10 08:54:44 +0200

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -7,3 +7,7 @@ timedatectl set-timezone Europe/Amsterdam
 apt upgrade -y
 apt dist-upgrade -y
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config_backup
+cp /etc/default/grub /etc/default/grub_backup
+sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
+echo "GRUB_HIDDEN_TIMEOUT_QUIET=true" >> /etc/default/grub
+update-grub
