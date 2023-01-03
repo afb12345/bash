d1908b85e12456c9e04596ab74fb7c1b7708b52d
afb12345 <62421730+afb12345@users.noreply.github.com>
2023-01-03 14:39:51 +0100

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -2,14 +2,18 @@
 
 read -p "IP/sn? " ip
 echo $ip
+read -p "GW? " gw
+echo $gw
+read -p "user? " user
+echo $user
 
 apt update
 apt install unattended-upgrades curl wget sudo nfs-common gnupg nano -y
 dpkg-reconfigure --priority=low unattended-upgrades
 apt upgrade -y
 
-/usr/sbin/runuser -l gfy -c "mkdir ~/.ssh && chmod 700 ~/.ssh"
-/usr/sbin/runuser -l gfy -c "chmod 700 ~/.ssh"
+/usr/sbin/runuser -l $user -c "mkdir ~/.ssh && chmod 700 ~/.ssh"
+/usr/sbin/runuser -l $user -c "chmod 700 ~/.ssh"
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config_backup
 cp /etc/default/grub /etc/default/grub_backup
 sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
@@ -17,4 +21,4 @@ update-grub
 
 sed -i 's/inet dhcp/inet static/' /etc/network/interfaces
 echo "   address $ip" >> /etc/network/interfaces
-echo "   gateway 10.10.0.1" >> /etc/network/interfaces
+echo "   gateway $gw" >> /etc/network/interfaces
