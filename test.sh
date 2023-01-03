1a5845daa46db06c25c837eda9270e9185056864
afb12345 <62421730+afb12345@users.noreply.github.com>
2023-01-03 20:52:34 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 read -p "key? " key
-/usr/sbin/runuser -l $user -c "echo 'ssh-rsa $key' >> ~/.ssh/test"
+/usr/sbin/runuser -l $user -c "echo 'ssh-rsa $key' >> /home/$user/.ssh/authorized_keys"
