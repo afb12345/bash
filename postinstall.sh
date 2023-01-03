b84bef3057afb103f8e788ff9e325601dfc54b77
afb12345 <62421730+afb12345@users.noreply.github.com>
2023-01-03 14:11:00 +0100

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,38 +1,18 @@
 #!/bin/sh
 
-#echo "Username 1?"
-#read user1
-#echo "Username 2?"
-#read user2
-
-#adduser $user1
-#adduser $user2
 read -p "IP/sn? " ip
 echo $ip
-read -p "Gateway? " ip
-echo $gw
 
 sudo apt update
 sudo apt install unattended-upgrades curl wget sudo nfs-common gnupg nano -y
 sudo dpkg-reconfigure --priority=low unattended-upgrades
 sudo apt upgrade -y
 
-#usermod -aG sudo $user1
-#usermod -aG sudo $user2
-#runuser -l $user1 -c "mkdir ~/.ssh && chmod 700 ~/.ssh"
-#runuser -l $user2 -c "mkdir ~/.ssh && chmod 700 ~/.ssh"
-
 mkdir ~/.ssh
 chmod 700 ~/.ssh
 sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config_backup
 sudo cp /etc/default/grub /etc/default/grub_backup
 sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
-sudo echo "GRUB_HIDDEN_TIMEOUT_QUIET=true" >> /etc/default/grub
 sudo update-grub
 
-sudo sed -i 's/inet dhcp/inet static
-   address $ip
-   gateway $gw   
-/' /etc/network/interfaces
-#sudo echo "   address $ip" >> /etc/network/interfaces
-#sudo echo "   gateway $gw" >> /etc/network/interfaces
+sudo sed -i 's/   address 10.10.1.10/   address $ip/' /etc/network/interfaces
