3a812913f6d27b1496b55d4e1cbf6d80f5b3a13c
afb12345 <62421730+afb12345@users.noreply.github.com>
2023-01-03 12:53:02 +0100

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-echo "Username 1?"
-read user1
-echo "Username 2?"
-read user2
+#echo "Username 1?"
+#read user1
+#echo "Username 2?"
+#read user2
 
-adduser $user1
-adduser $user2
+#adduser $user1
+#adduser $user2
 
 echo "IP?"
 read $ip
@@ -14,15 +14,17 @@ echo "Gateway?"
 read $gw
 
 apt update
-apt install unattended-upgrades curl sudo nfs-common gnupg -y
+apt install unattended-upgrades curl wget sudo nfs-common gnupg nano -y
 dpkg-reconfigure --priority=low unattended-upgrades
 apt upgrade -y
 
-usermod -aG sudo $user1
-usermod -aG sudo $user2
-runuser -l $user1 -c "mkdir ~/.ssh && chmod 700 ~/.ssh"
-runuser -l $user2 -c "mkdir ~/.ssh && chmod 700 ~/.ssh"
+#usermod -aG sudo $user1
+#usermod -aG sudo $user2
+#runuser -l $user1 -c "mkdir ~/.ssh && chmod 700 ~/.ssh"
+#runuser -l $user2 -c "mkdir ~/.ssh && chmod 700 ~/.ssh"
 
+mkdir ~/.ssh
+chmod 700 ~/.ssh
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config_backup
 cp /etc/default/grub /etc/default/grub_backup
 sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
