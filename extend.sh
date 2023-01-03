84e30cd7268111ec99af2643886d54b3cb73aacb
afb12345 <62421730+afb12345@users.noreply.github.com>
2023-01-03 22:29:15 +0100

    Update extend.sh

--- extend.sh
+++ extend.sh
@@ -1,13 +1,15 @@
-sudo fdisk /dev/vda
-n
-default p
-default 3
-default
-default
-t
-3
-8e
-w
+echo "label: dos
+label-id: 0x3b59eaa5
+device: /dev/vda
+unit: sectors
+sector-size: 512
+
+/dev/vda1 : start=        2048, size=      997376, type=83, bootable
+/dev/vda2 : start=     1001470, size=    15773698, type=5
+/dev/vda3 : start=    16775168, size=    16779264, type=8e
+/dev/vda5 : start=     1001472, size=    15773696, type=8e" >> partition
+sudo sfdisk /dev/vda < partition --force
+sudo partx -a /dev/vda3 /dev/vda
 sudo pvcreate /dev/vda3
 sudo vgextend debian-vg /dev/vda3
 sudo pvscan
