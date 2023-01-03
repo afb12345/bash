368df0054bf0e836a6194601d3193d03d9e82874
afb12345 <62421730+afb12345@users.noreply.github.com>
2023-01-03 14:32:24 +0100

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -3,16 +3,18 @@
 read -p "IP/sn? " ip
 echo $ip
 
-sudo apt update
-sudo apt install unattended-upgrades curl wget sudo nfs-common gnupg nano -y
-sudo dpkg-reconfigure --priority=low unattended-upgrades
-sudo apt upgrade -y
+apt update
+apt install unattended-upgrades curl wget sudo nfs-common gnupg nano -y
+dpkg-reconfigure --priority=low unattended-upgrades
+apt upgrade -y
 
-mkdir ~/.ssh
-chmod 700 ~/.ssh
-sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config_backup
-sudo cp /etc/default/grub /etc/default/grub_backup
-sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
-sudo update-grub
+runuser -l gfy -c "mkdir ~/.ssh && chmod 700 ~/.ssh"
+runuser -l gfy -c "chmod 700 ~/.ssh"
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config_backup
+cp /etc/default/grub /etc/default/grub_backup
+sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
+update-grub
 
-sudo sed -i 's/   address 10.10.1.10/   address $ip/' /etc/network/interfaces
+sed -i 's/inet dhcp/inet static/' /etc/network/interfaces
+echo "   address $ip" >> /etc/network/interfaces
+echo "   gateway 10.10.0.1" >> /etc/network/interfaces
