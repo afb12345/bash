5f7903c87733ab09de075aec298017b3f2c6d4c7
afb12345 <62421730+afb12345@users.noreply.github.com>
2023-01-03 14:42:44 +0100

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -9,7 +9,7 @@ echo $user
 
 apt update
 apt install unattended-upgrades curl wget sudo nfs-common gnupg nano -y
-dpkg-reconfigure --priority=low unattended-upgrades
+/usr/sbin/dpkg-reconfigure --priority=low unattended-upgrades
 apt upgrade -y
 
 /usr/sbin/runuser -l $user -c "mkdir ~/.ssh && chmod 700 ~/.ssh"
@@ -17,7 +17,7 @@ apt upgrade -y
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config_backup
 cp /etc/default/grub /etc/default/grub_backup
 sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
-update-grub
+/usr/sbin/update-grub
 
 sed -i 's/inet dhcp/inet static/' /etc/network/interfaces
 echo "   address $ip" >> /etc/network/interfaces
