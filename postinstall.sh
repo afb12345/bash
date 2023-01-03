d99d864dd5ce72b002fe21c201559a336931b2b8
afb12345 <62421730+afb12345@users.noreply.github.com>
2023-01-03 14:38:42 +0100

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -8,8 +8,8 @@ apt install unattended-upgrades curl wget sudo nfs-common gnupg nano -y
 dpkg-reconfigure --priority=low unattended-upgrades
 apt upgrade -y
 
-runuser -l gfy -c "mkdir ~/.ssh && chmod 700 ~/.ssh"
-runuser -l gfy -c "chmod 700 ~/.ssh"
+/usr/sbin/runuser -l gfy -c "mkdir ~/.ssh && chmod 700 ~/.ssh"
+/usr/sbin/runuser -l gfy -c "chmod 700 ~/.ssh"
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config_backup
 cp /etc/default/grub /etc/default/grub_backup
 sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
