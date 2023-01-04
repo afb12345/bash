3db1855bc09fe1ee0c2fd44767319e816662bff1
afb12345 <62421730+afb12345@users.noreply.github.com>
2023-01-04 12:23:17 +0100

    Update extend.sh

--- extend.sh
+++ extend.sh
@@ -9,7 +9,7 @@ max=$((sectors-1))
 sizen=$((max-endp))
 
 echo "START VDA3"
-echo $start
+echo $startn
 echo "MAX SIZE"
 echo $max
 echo "SIZE VDA3"
@@ -25,10 +25,10 @@ sector-size: 512
 /dev/vda2 : start=     1001470, size=    15773698, type=5
 /dev/vda3 : start=    $startn, size=    $sizen, type=8e
 /dev/vda5 : start=     1001472, size=    15773696, type=8e" >> partition
-#sudo sfdisk /dev/vda < partition --force
-#sudo partx -a /dev/vda3 /dev/vda
-#sudo pvcreate /dev/vda3
-#sudo vgextend debian-vg /dev/vda3
-#sudo pvscan
-#sudo lvextend /dev/debian-vg/root /dev/vda3
-#sudo resize2fs /dev/debian-vg/root
+sudo sfdisk /dev/vda < partition --force
+sudo partx -a /dev/vda3 /dev/vda
+sudo pvcreate /dev/vda3
+sudo vgextend debian-vg /dev/vda3
+sudo pvscan
+sudo lvextend /dev/debian-vg/root /dev/vda3
+sudo resize2fs /dev/debian-vg/root
