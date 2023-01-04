ae0f8945f402a8ea9d8398e89104f0cd6fcf21b9
afb12345 <62421730+afb12345@users.noreply.github.com>
2023-01-04 12:13:27 +0100

    Update extend.sh

--- extend.sh
+++ extend.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
+echo "##############################################################################"
+sudo fdisk -l
+echo "##############################################################################"
+read -p "end VDA5? " endp
+read -p "total sectors " sectors
+startn=$((endp+1))
+max=$((sectors-1))
+sizen=$((max-endp))
 
-fdisk -l -x /dev/vda5
-
-read -p "start VDA5? " start
-read -p "size VDA5? " size
-newsector=$((start+size))
-echo $newsector
+echo "START VDA3"
+echo $start
+echo "MAX SIZE"
+echo $max
+echo "SIZE VDA3"
+echo $sizen
 
 echo "label: dos
 label-id: 0x3b59eaa5
@@ -15,12 +23,12 @@ sector-size: 512
 
 /dev/vda1 : start=        2048, size=      997376, type=83, bootable
 /dev/vda2 : start=     1001470, size=    15773698, type=5
-/dev/vda3 : start=    16775168, size=    16779264, type=8e
+/dev/vda3 : start=    $startn, size=    $sizen, type=8e
 /dev/vda5 : start=     1001472, size=    15773696, type=8e" >> partition
-sudo sfdisk /dev/vda < partition --force
-sudo partx -a /dev/vda3 /dev/vda
-sudo pvcreate /dev/vda3
-sudo vgextend debian-vg /dev/vda3
-sudo pvscan
-sudo lvextend /dev/debian-vg/root /dev/vda3
-sudo resize2fs /dev/debian-vg/root
+#sudo sfdisk /dev/vda < partition --force
+#sudo partx -a /dev/vda3 /dev/vda
+#sudo pvcreate /dev/vda3
+#sudo vgextend debian-vg /dev/vda3
+#sudo pvscan
+#sudo lvextend /dev/debian-vg/root /dev/vda3
+#sudo resize2fs /dev/debian-vg/root
